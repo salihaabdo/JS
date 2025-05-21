c8ee9352c7c9a80807e2b36e2eeca8fa48bae5dc
salihaabdo <saloooalfakiii13@gmail.com>
2025-05-21 16:34:36 +0300

    Add files via upload

---
+++ template_1/script.js
@@ -0,0 +1,6 @@
+
+function showAlertSEEPROJECT() {
+    alert("See Project button have been clicked!");
+} 
+function showAlertGETMYRESUME() {
+    alert("Ger My Resume button have been clicked!");}
